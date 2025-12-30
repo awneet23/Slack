eb44a2651388633a0c55eedd85aaedf1986f29f3
awneet23 <awneet23@gmail.com>
2025-12-30 15:18:14 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {ENV} from "./config/env.js"
+
+
+
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello world123");
+});
+
+
+
+app.listen(ENV.PORT, () => {
+  console.log(`Server running on port ${ENV.PORT}`);
+  
+});
+
+
+
+
+
+
+// 4xxNrhdYT91Fkisd
