e3d9083cc66f5f89a8fb818e162ddab09d073cc2
awneet23 <awneet23@gmail.com>
2025-12-30 19:30:56 +0530

    syncing main and auth

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,24 +1,44 @@
 import express from "express";
 import {ENV} from "./config/env.js"
 import { connectDB } from "./config/db.js";
-
+import {clerkMiddleware} from "@clerk/express";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./src/inngest"
 
 
 const app = express();
+app.use(clerkMiddleware());
+
+app.use(express.json())
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => {
+  
   res.send("hello world123");
 });
 
 
 
-app.listen(ENV.PORT, () => {
+const startServer = () =>{
+  try{
+    await connectDB();
+    if (ENV.NODE_ENV !== 'production'){
+      app.listen(ENV.PORT, () => {
   console.log(`Server running on port ${ENV.PORT}`);
-  connectDB(); 
+  
 });
 
 
 
+    }
+  }
+  catch(err){
+    console.log(err);
+  }
+};
+
+export default app;
 
 
 
