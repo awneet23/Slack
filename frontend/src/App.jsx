167e18598bc695f4f477cdebe44d6ec592fcc4df
awneet23 <awneet23@gmail.com>
2025-12-31 14:43:51 +0530

    auth page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,27 @@ import {
   SignInButton,
   UserButton,
 } from "@clerk/clerk-react";
+import { Routes, Route, Navigate } from "react-router";
+import HomePage from "./pages/HomePage";
+import AuthPage from "./pages/AuthPage";
 function App() {
   return (
-    <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
+    <>
       <SignedIn>
+        <Routes>
+          <Route path="/" element={<HomePage></HomePage>}></Route>
+          <Route path="/auth" element={<Navigate to={"/"} />}></Route>
+        </Routes>
         <UserButton />
       </SignedIn>
-    </header>
+      <SignedOut>
+        <Routes>
+          <Route path="/auth" element={<AuthPage></AuthPage>}></Route>
+          <Route path="*" element={<Navigate to={"/auth"} />}></Route>
+        </Routes>
+        <SignInButton />
+      </SignedOut>
+    </>
   );
 }
 export default App;
