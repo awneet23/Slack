955fe87af115f8c59c9fcc07dfe6475dd70c1df0
awneet23 <awneet23@gmail.com>
2025-12-31 16:27:16 +0530

    fixed axios issue

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE==='development' ? "https://localhost:5001/api": "https://slackbackend-cmqdoubk8-awneets-projects.vercel.app/api";
+const BASE_URL = import.meta.env.MODE==='development' ? "http://localhost:5001/api": "https://slackbackend-cmqdoubk8-awneets-projects.vercel.app/api";
 
 export const axiosInstance = axios.create({
 baseURL:BASE_URL,
